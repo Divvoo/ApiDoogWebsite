80a8db82bbf8e5777d425d6bae2c695d1b87e795
Dawid <dawidrowinski@vp.pl>
2023-08-23 15:48:53 +0200

    init commit

---
+++ Script.js
@@ -0,0 +1,22 @@
+const długopis=50
+// console.log(długopis)
+const div=document.getElementById("klawisz")
+// console.log(div)
+const button=document.getElementById("button")
+button.addEventListener("click", function(){
+    div.innerHTML="samochód"
+})
+async function logMovies(){
+    // const respan1 = fetch("https://dog.ceo/api/breeds/image/random");
+    // console.log(respan1)
+
+    const respan2 = await fetch("https://dog.ceo/api/breeds/image/random");
+    const movies = await respan2.json()
+    console.log(movies)
+    document.getElementById("image").innerHTML = `<p>String text ${movies.message}</p>`
+}
+
+
+
+
+
